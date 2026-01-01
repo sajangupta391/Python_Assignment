2c8729e41a93beb020da2e4c76edb293aaf67910
Adarash Gupta <sajangupta391@gmail.com>
2026-01-01 19:27:51 +0530

    Add files via upload

---
+++ prime_number.py
@@ -0,0 +1,27 @@
+import math
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    
+    if n == 2:
+        return True
+    
+    if n % 2 == 0:
+        return False
+    
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+            
+    return True
+
+try:
+    num = int(input("Enter a number to check: "))
+    
+    if is_prime(num):
+        print(f"{num} is a prime number.")
+    else:
+        print(f"{num} is not a prime number.")
+except ValueError:
+    print("Invalid input! Please enter an integer.")
