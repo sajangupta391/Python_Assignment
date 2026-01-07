22866f5eb8bc4602bf673459e3a8ebf6eea0094f
Adarash Gupta <sajangupta391@gmail.com>
2026-01-07 19:12:15 +0530

    Add files via upload

---
+++ calculate_factorial.py
@@ -0,0 +1,20 @@
+# Question 5
+
+def calculate_factorial(n):
+    """
+    Calculates the factorial of non-negative int.
+    """
+    if not isinstance(n, int):
+        return "Error: Input must be int."
+
+    if n < 0:
+        return "Error: Input must be non-negative int."
+
+    if n == 0:
+        return 1
+
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+print(f"Factorial :", {calculate_factorial(4.5)})
